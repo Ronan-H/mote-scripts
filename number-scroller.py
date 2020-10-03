127adb0ff66878cf199039e4a7f0015953720b94
ronan <g00342393@gmit.ie>
2020-10-03 10:58:14 +0100

    Number scroller is now working

--- number-scroller.py
+++ number-scroller.py
@@ -3,9 +3,6 @@ from mote import Mote
 from time import sleep
 from PIL import Image
 
-# time to sleep after checking the API
-sleep_time = 5 * 60
-
 
 def new_cases_today():
     data_retrieved = False
@@ -20,7 +17,6 @@ def new_cases_today():
     tot_cases_today = int(data[-1]['Cases'])
     tot_cases_yesterday = int(data[-2]['Cases'])
     num_cases_today = tot_cases_today - tot_cases_yesterday
-    print('Updated new cases today:', num_cases_today)
     return num_cases_today
 
 
@@ -32,7 +28,6 @@ def gen_char_mappings():
     pixels_per_char = img_width * img_height
     pixels = [x for sets in list(char_img.getdata()) for x in sets][3::4]
     mapping = dict()
-    print(pixels)
     for i, char in enumerate(char_mapping):
         on_indexes = [[] for _ in range(img_height)]
         index = i * pixels_per_char
@@ -45,27 +40,32 @@ def gen_char_mappings():
 
 
 char_mappings = gen_char_mappings()
-print(char_mappings)
 
 mote = Mote()
 
-mote.configure_channel(1, 16, False)
+for channel in range(1, 5):
+    mote.configure_channel(channel, 16, False)
+
 last_new_cases = None
 
 mote.clear()
 
-scrolls_until_refresh = 50
+scrolls_until_refresh = 25
 scrolls_left = -1
 scroll_y = 0
-scroll_sleep = 0.25
+scroll_sleep = 0.08
 spacing = 1
+
+colour_cycle = ((0, 255, 0), (255, 255, 255), (255, 100, 0))
+colour_index = -1
 
 display_text = None
 
 while True:
-    if display_text is None or scroll_y > len(display_text) * 6 * 2:  # TODO work out when scroll has reached end
+    if display_text is None or scroll_y > len(display_text) * 5 * 1.5:  # TODO work out when scroll has reached end
         scrolls_left -= 1
-        scroll_y = -3
+        scroll_y = -17
+        colour_index = (colour_index + 1) % len(colour_cycle)
         if scrolls_left <= 0:
             print('Retrieving today\'s new cases from API...')
             new_cases = new_cases_today()
@@ -75,20 +75,17 @@ while True:
             scrolls_left = scrolls_until_refresh
 
     y = scroll_y
-    for c in display_text:
+    mote.clear()
+    for ci, c in enumerate(display_text):
         pixels = char_mappings[c]
 
         for py, row in enumerate(pixels):
             for px, pixel_on in enumerate(row):
-                y_loc = y + py
-                if 0 <= y_loc <= 15 and pixel_on:
-                    mote.set_pixel(px + 1, y_loc, 255, 255, 255, 0.05)
-                else:
-                    mote.set_pixel(px + 1, y_loc, 0, 0, 0, 0)
-        y += len(pixels) + spacing
+                y_loc = y - py + 16
+                if 0 <= y_loc <= 15:
+                    if pixel_on:
+                        mote.set_pixel(px + 1, y_loc, *colour_cycle[(colour_index + ci) % 3], 0.05)
+        y -= len(pixels) + spacing
     mote.show()
     scroll_y += 1
     sleep(scroll_sleep)
-
-    print('Sleeping for {} seconds...'.format(sleep_time))
-    sleep(sleep_time)
